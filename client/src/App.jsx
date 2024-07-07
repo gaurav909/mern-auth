c952465fc23fd0e10e403d8a690e227f423a823e
gaurav909 <gaurav150702@gmail.com>
2024-07-07 12:04:00 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SingIn from './pages/SingIn'
+import SingnUp from './pages/SingnUp'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className="text-red-600">App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<SingIn/>}/>
+      <Route path='/sign-up' element={<SingnUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/SingnUp.jsx
@@ -0,0 +1,9 @@
+
+
+const SingnUp = () => {
+  return (
+    <div>SingnUp</div>
+  )
+}
+
+export default SingnUp
